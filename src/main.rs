754d0ad18676db142436eee22955e0908749c049
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2019-01-26 19:44:27 +0900

    Add schreier_sims

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::collections::VecDeque;
 
 // Reference: https://blogs.cs.st-andrews.ac.uk/codima/files/2015/11/CoDiMa2015_Holt.pdf
 #[derive(Clone, Eq, PartialEq, Debug, PartialOrd, Ord)]
-struct Perm(Vec<usize>);
+pub struct Perm(Vec<usize>);
 impl Perm {
     fn new(perm: Vec<usize>) -> Perm {
         let n = perm.len();
@@ -39,7 +39,7 @@ impl Perm {
         }
         Self::new(ans)
     }
-    fn pow(&self, k: i64) -> Self {
+    pub fn pow(&self, k: i64) -> Self {
         let n = self.size();
         let mut sum = Self::e(n);
         if k == 0 {
@@ -67,17 +67,24 @@ impl std::ops::Index<usize> for Perm {
     }
 }
 
+// A collection of pairs (x, alpha) s.t. v^alpha = x.
+type OrbitTransversal = Vec<(usize, Perm)>;
+
+// An oracle of type usize -> Perm.
+type Transversal = Vec<Perm>;
+
 /// gen: generators, v: stabilized point
 ///
 /// This function returns a generator set of the stabilizer group G_v = Stab_G(v).
-fn schreier_theorem(n: usize, gen: &[Perm], v: usize) -> (usize, Vec<Perm>) {
+fn orbit_transversal_stabilizer(n: usize, gen: &[Perm], v: usize)
+                                -> (OrbitTransversal, Vec<Perm>) {
     let mut stabilizer_gen = Vec::new();
     // Calculates a variant of the Schreier vector
     // performing breadth-first search.
     let mut que = VecDeque::new();
     que.push_back((v, Perm::e(n)));
     let mut table: Vec<Option<Perm>> = vec![None; n];
-    let mut orbit = 0;
+    let mut orbit_transversal = vec![];
     while let Some((w, p)) = que.pop_front() {
         if let Some(ref q) = table[w] {
             // r = qp^{-1} stabilizes v.
@@ -90,8 +97,8 @@ fn schreier_theorem(n: usize, gen: &[Perm], v: usize) -> (usize, Vec<Perm>) {
                 // move to w^x (x applied to w).
                 que.push_back((x[w], p.compose(x)));
             }
+            orbit_transversal.push((w, p.clone()));
             table[w] = Some(p);
-            orbit += 1;
         }
     }
     // Remove duplicate generators
@@ -99,17 +106,74 @@ fn schreier_theorem(n: usize, gen: &[Perm], v: usize) -> (usize, Vec<Perm>) {
     stabilizer_gen.dedup();
 
     // Returns ((G : G_v), a generator set of G_v)
-    (orbit, stabilizer_gen)
+    (orbit_transversal, stabilizer_gen)
+}
+
+fn strip(g: &Perm, beta_transversals: &[(usize, Transversal)]) -> (Vec<Perm>, Perm) {
+    let mut h = g.clone();
+    let mut us = vec![];
+    for i in 0 .. beta_transversals.len() {
+        let (beta, ref transversal) = beta_transversals[i];
+        let moved_to = h[beta];
+        eprintln!("trans = {:?}, {} -> {}", transversal, beta, moved_to);
+        let repr = &transversal[moved_to];
+        h = h.compose(&repr.inv());
+        us.push(repr.clone());
+    }
+    (us, h)
+}
+
+fn schreier_sims(n: usize, beta_transversals: &[(usize, Transversal)],
+                 s: &[Perm]) -> Result<(), (Vec<Perm>, Perm)> {
+    if beta_transversals.len() == 0 {
+        if s.len() == 0 {
+            return Ok(());
+        }
+        return Err((vec![], s[0].clone()));
+    }
+    let mut intersection = Vec::new();
+    // The first fixed point
+    let beta0 = beta_transversals[0].0;
+    for s in s {
+        if s[beta0] == beta0 {
+            intersection.push(s.clone());
+        }
+    }
+    schreier_sims(n, &beta_transversals[1..], &intersection)?;
+    let (_orbit, y) = orbit_transversal_stabilizer(n, s, beta0);
+    for y in y {
+        let (u, h) = strip(&y, &beta_transversals[1..]);
+        if h != Perm::e(n) {
+            return Err((u, h));
+        }
+    }
+    Ok(())
 }
 
 fn main() {
-    let gen = vec![Perm::new(vec![1, 2, 3, 0]), Perm::new(vec![1, 0, 2, 3])];
-    let (h1, stab1) = schreier_theorem(4, &gen, 0);
-    println!("{:?}", (h1, &stab1));
-    let (h2, stab2) = schreier_theorem(4, &stab1, 1);
-    println!("{:?}", (h2, &stab2));
-    let (h3, stab3) = schreier_theorem(4, &stab2, 2);
-    println!("{:?}", (h3, &stab3));
+    // G= <(0 1 2), (2 3 4)>
+    let n = 5;
+    let gen = vec![Perm::new(vec![1, 2, 0, 3, 4]),
+                   Perm::new(vec![0, 1, 3, 4, 2])];
+    let beta = vec![0, 2];
+    let (orbit_transversal0, subgen) =
+        orbit_transversal_stabilizer(n, &gen, beta[0]);
+    let mut transversal0 = vec![Perm::e(0); n];
+    for (point, trans) in orbit_transversal0 {
+        transversal0[point] = trans;
+    }
+    eprintln!("transversal0 = {:?}", transversal0);
+    let (orbit_transversal1, _) =
+        orbit_transversal_stabilizer(n, &subgen, beta[1]);
+    let mut transversal1 = vec![Perm::e(0); n];
+    for (point, trans) in orbit_transversal1 {
+        transversal1[point] = trans;
+    }
+    eprintln!("transversal1 = {:?}", transversal1);
+
+    let beta_transversals = vec![(beta[0], transversal0), (beta[1], transversal1)];
+    let ans = schreier_sims(5, &beta_transversals, &gen);
+    eprintln!("ans = {:?}", ans);
 }
 
 #[cfg(test)]
@@ -128,17 +192,53 @@ mod tests {
         assert_eq!(p.pow(-3), p.compose(&p));
     }
     #[test]
-    fn schreier_theorem_test() {
+    fn orbit_transversal_stabilizer_test() {
         let gen = vec![Perm::new(vec![1, 2, 3, 0]), Perm::new(vec![1, 0, 2, 3])];
-        let (h1, stab1) = schreier_theorem(4, &gen, 0);
+        let (h1, stab1) = orbit_transversal_stabilizer(4, &gen, 0);
         // |0^G| = 4, H^{(1)} = G_0 = <(2 3), (1 2)>
-        assert_eq!(h1, 4);
-        let (h2, stab2) = schreier_theorem(4, &stab1, 1);
+        assert_eq!(h1.len(), 4);
+        let (h2, stab2) = orbit_transversal_stabilizer(4, &stab1, 1);
         // |1^{H^{(1)}}| = 3, H^{(2)} = H^{(1)}_1 = <(2 3)>
-        assert_eq!(h2, 3);
-        let (h3, stab3) = schreier_theorem(4, &stab2, 2);
+        assert_eq!(h2.len(), 3);
+        let (h3, stab3) = orbit_transversal_stabilizer(4, &stab2, 2);
         // |2^{H^{(2)}}| = 2, H^{(3)} = H^{(2)}_2 = {e}
-        assert_eq!(h3, 2);
+        assert_eq!(h3.len(), 2);
         assert_eq!(stab3, Vec::new());
+    }
+    #[test]
+    fn schreier_sims_test() {
+        // G= <(0 1 2), (2 3 4)>
+        let n = 5;
+        let gen = vec![Perm::new(vec![1, 2, 0, 3, 4]),
+                       Perm::new(vec![0, 1, 3, 4, 2])];
+        let beta = vec![0, 2];
+        let (orbit_transversal0, subgen) =
+            orbit_transversal_stabilizer(n, &gen, beta[0]);
+        assert_eq!(orbit_transversal0.len(), 5);
+        let mut transversal0 = vec![Perm::e(0); n];
+        for (point, trans) in orbit_transversal0 {
+            transversal0[point] = trans;
+        }
+        let (orbit_transversal1, _) =
+            orbit_transversal_stabilizer(n, &subgen, beta[1]);
+        assert_eq!(orbit_transversal1.len(), 4);
+        let mut transversal1 = vec![Perm::e(0); n];
+        for (point, trans) in orbit_transversal1 {
+            transversal1[point] = trans;
+        }
+        // orbit 1^{G^{(1)}} is {1, 2, 3, 4}.
+        assert_eq!(transversal1[0], Perm::e(0));
+        
+        let beta_transversals =
+            vec![(beta[0], transversal0), (beta[1], transversal1)];
+        let ans = schreier_sims(5, &beta_transversals, &gen);
+        // (beta, gen) is not a BSGS:
+        // there is an element of G that needs appending to gen.
+        assert!(ans.is_err());
+        let (_, h) = ans.unwrap_err();
+        // Asserts h stabilizes 0 and 2.
+        for &point in &beta {
+            assert_eq!(h[point], point);
+        }
     }
 }
