53c9e9a54c265bfc4a1ecc8a2aabf6c4cccd0f40
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2019-01-29 23:36:43 +0900

    Add tests for Rubik's Cube group

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,43 @@
 use rust_schreier::perm::Perm;
 use rust_schreier::schreier::order;
 
-fn main() {
-    // Star, G = <(0 n-1), (1 n-1), ...> = S_n, |G| = n!
-    let n = 20;
-    let mut gen = vec![];
-    for i in 0..n - 1 {
-        let mut p = vec![0; n];
-        for j in 0..n {
-            p[j] = j;
+fn get_cycle(n: usize, a: &[Vec<usize>]) -> Perm {
+    let mut e = Perm::e(n);
+    for a in a {
+        let mut t: Vec<_> = (0..n).collect();
+        for i in 0..a.len(){
+            t[a[i]] = a[(i + 1) % a.len()];
         }
-        p.swap(i, n - 1);
-        gen.push(Perm::new(p));
+        e = e.compose(&Perm::new(t));
     }
+    e
+}
+
+fn main() {
+    // Rubik's Cube group
+    let n = 48;
+    let mut gen = vec![];
+    let p0 = vec![vec![0, 2, 4, 6], vec![1, 3, 5, 7],
+                  vec![14, 16, 34, 28], vec![13, 23, 33, 27],
+                  vec![12, 22, 32, 26]];
+    let p2 = vec![vec![16, 18, 20, 22], vec![17, 19, 21, 23],
+                  vec![12, 40, 36, 4], vec![11, 47, 35, 3],
+                  vec![10, 46, 34, 2]];
+    let p1 = vec![vec![8, 10, 12, 14], vec![9, 11, 13, 15],
+                  vec![42, 18, 2, 26], vec![41, 17, 1, 25],
+                  vec![40, 16, 0, 24]];
+    let p3 = vec![vec![24, 26, 28, 30], vec![25, 27, 29, 31],
+                  vec![8, 0, 32, 44], vec![15, 7, 39, 43],
+                  vec![14, 6, 38, 42]];
+    let p4 = vec![vec![32, 34, 36, 38], vec![33, 35, 37, 39],
+                  vec![6, 22, 46, 30], vec![5, 21, 45, 29],
+                  vec![4, 20, 44, 28]];
+    let p5 = vec![vec![40, 42, 44, 46], vec![41, 43, 45, 47],
+                  vec![10, 24, 38, 20], vec![9, 31, 37, 19],
+                  vec![8, 30, 36, 18]];
+    for p in &[p0, p1, p2, p3, p4, p5] {
+        gen.push(get_cycle(n, p));
+    }
+    // Outputs order = 43252003274489856000
     eprintln!("order = {}", order(n, &gen));
 }
