9e265c95a2bc044b6eecf13fa02d235aaf120f88
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2019-01-29 23:50:58 +0900

    cargo fmt + cargo clippy

--- src/schreier.rs
+++ src/schreier.rs
@@ -120,8 +120,8 @@ pub fn order(n: usize, gen: &[Perm]) -> num_bigint::BigInt {
     let (beta_transversals, _) = incrementally_build_bsgs(n, &[], gen, &mut rnd);
     for (_, transversal) in beta_transversals {
         let mut u = 0;
-        for i in 0..n {
-            if let Some(_) = transversal[i] {
+        for transversal in transversal {
+            if transversal.is_some() {
                 u += 1;
             }
         }
@@ -230,7 +230,7 @@ mod tests {
         let mut e = Perm::e(n);
         for a in a {
             let mut t: Vec<_> = (0..n).collect();
-            for i in 0..a.len(){
+            for i in 0..a.len() {
                 t[a[i]] = a[(i + 1) % a.len()];
             }
             e = e.compose(&Perm::new(t));
@@ -243,28 +243,54 @@ mod tests {
         // Rubik's Cube group
         let n = 48;
         let mut gen = vec![];
-        let p0 = vec![vec![0, 2, 4, 6], vec![1, 3, 5, 7],
-                      vec![14, 16, 34, 28], vec![13, 23, 33, 27],
-                      vec![12, 22, 32, 26]];
-        let p2 = vec![vec![16, 18, 20, 22], vec![17, 19, 21, 23],
-                      vec![12, 40, 36, 4], vec![11, 47, 35, 3],
-                      vec![10, 46, 34, 2]];
-        let p1 = vec![vec![8, 10, 12, 14], vec![9, 11, 13, 15],
-                      vec![42, 18, 2, 26], vec![41, 17, 1, 25],
-                      vec![40, 16, 0, 24]];
-        let p3 = vec![vec![24, 26, 28, 30], vec![25, 27, 29, 31],
-                      vec![8, 0, 32, 44], vec![15, 7, 39, 43],
-                      vec![14, 6, 38, 42]];
-        let p4 = vec![vec![32, 34, 36, 38], vec![33, 35, 37, 39],
-                      vec![6, 22, 46, 30], vec![5, 21, 45, 29],
-                      vec![4, 20, 44, 28]];
-        let p5 = vec![vec![40, 42, 44, 46], vec![41, 43, 45, 47],
-                      vec![10, 24, 38, 20], vec![9, 31, 37, 19],
-                      vec![8, 30, 36, 18]];
+        let p0 = vec![
+            vec![0, 2, 4, 6],
+            vec![1, 3, 5, 7],
+            vec![14, 16, 34, 28],
+            vec![13, 23, 33, 27],
+            vec![12, 22, 32, 26],
+        ];
+        let p2 = vec![
+            vec![16, 18, 20, 22],
+            vec![17, 19, 21, 23],
+            vec![12, 40, 36, 4],
+            vec![11, 47, 35, 3],
+            vec![10, 46, 34, 2],
+        ];
+        let p1 = vec![
+            vec![8, 10, 12, 14],
+            vec![9, 11, 13, 15],
+            vec![42, 18, 2, 26],
+            vec![41, 17, 1, 25],
+            vec![40, 16, 0, 24],
+        ];
+        let p3 = vec![
+            vec![24, 26, 28, 30],
+            vec![25, 27, 29, 31],
+            vec![8, 0, 32, 44],
+            vec![15, 7, 39, 43],
+            vec![14, 6, 38, 42],
+        ];
+        let p4 = vec![
+            vec![32, 34, 36, 38],
+            vec![33, 35, 37, 39],
+            vec![6, 22, 46, 30],
+            vec![5, 21, 45, 29],
+            vec![4, 20, 44, 28],
+        ];
+        let p5 = vec![
+            vec![40, 42, 44, 46],
+            vec![41, 43, 45, 47],
+            vec![10, 24, 38, 20],
+            vec![9, 31, 37, 19],
+            vec![8, 30, 36, 18],
+        ];
         for p in &[p0, p1, p2, p3, p4, p5] {
             gen.push(get_cycle(n, p));
         }
-        assert_eq!(order(n, &gen),
-                   num_bigint::BigInt::from_str("43252003274489856000").unwrap());
+        assert_eq!(
+            order(n, &gen),
+            num_bigint::BigInt::from_str("43252003274489856000").unwrap()
+        );
     }
 }
